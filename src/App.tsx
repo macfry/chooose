c1931135a4fe4a85c85f67f48217dc2f5fd65095
Maciej Fraj <maciej.fraj@yahoo.co.uk>
2023-06-30 20:11:39 +0200

    add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, ChakraProvider, Container, SimpleGrid } from '@chakra-ui/react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Container maxW='2xl'>
+        <SimpleGrid minChildWidth='120px' spacing='40px'>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+        </SimpleGrid>
+      </Container>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
